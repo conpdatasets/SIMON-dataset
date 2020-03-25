9cde4fc010f36d16182cb9457c7f230495324adb
Tristan Glatard <tristan.glatard@concordia.ca>
2020-03-25 15:05:22 -0400

    [DATALAD] Recorded changes

--- crawl.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-URLPRE="https://amnesia.cbrain.mcgill.ca/SIMON_data/SIMON_BIDS/"
-FILEPRE="./data_BIDS"
-for file in $(awk -F 'SIMON_BIDS/' '{print $2}' all_urls_clean.txt)
-do
-    echo git-annex addurl --fast ${URLPRE}/${file} --file ${FILEPRE}/${file}
-done
